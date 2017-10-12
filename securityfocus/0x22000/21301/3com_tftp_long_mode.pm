5d723701df8869e72854531285af54516bf6a177
ox1111 <31942498+ox1111@users.noreply.github.com>
2017-10-12 09:22:34 +0900

    2017.10.10

---
+++ securityfocus/0x22000/21301/3com_tftp_long_mode.pm
@@ -0,0 +1,7 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL /vulnerabilities/exploits/3com_tftp_long_mode.pm was not found on this server.</p>
+</body></html>
---
+++ securityfocus/0x23000/22886/PHP_import_reqest_variable_overwrite_test.sh
@@ -0,0 +1,7 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL /vulnerabilities/exploits/PHP_import_reqest_variable_overwrite_test.sh was not found on this server.</p>
+</body></html>
